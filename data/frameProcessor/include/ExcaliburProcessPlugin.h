6fe999108515bbb21c6de3f23eb2230846f1dced
Alan Greer <alan.greer@diamond.ac.uk>
2018-04-05 11:39:24 +0100

    Implemented requestConfiguration method for the excalibur process plugin.

--- data/frameProcessor/include/ExcaliburProcessPlugin.h
+++ data/frameProcessor/include/ExcaliburProcessPlugin.h
@@ -68,6 +68,7 @@ namespace FrameProcessor
     std::string get_version_long();
 
     void configure(OdinData::IpcMessage& config, OdinData::IpcMessage& reply);
+    void requestConfiguration(OdinData::IpcMessage& reply);
     void status(OdinData::IpcMessage& status);
     bool reset_statistics(void);
 
