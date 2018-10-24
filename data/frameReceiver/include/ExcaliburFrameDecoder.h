fe042e94174d44760954db3d8aeecbdc1b3cba56
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2018-10-24 14:04:16 +0100

    Add version reporting to decoder and process plugins

--- data/frameReceiver/include/ExcaliburFrameDecoder.h
+++ data/frameReceiver/include/ExcaliburFrameDecoder.h
@@ -41,6 +41,12 @@ namespace FrameReceiver
     ExcaliburFrameDecoder();
     ~ExcaliburFrameDecoder();
 
+    int get_version_major();
+    int get_version_minor();
+    int get_version_patch();
+    std::string get_version_short();
+    std::string get_version_long();
+
     void init(LoggerPtr& logger, OdinData::IpcMessage& config_msg);
     void request_configuration(const std::string param_prefix, OdinData::IpcMessage& config_reply);
 
