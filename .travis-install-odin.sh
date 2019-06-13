0d5d093d32ab6c0481061bca42fab3f9d56c7794
Sky French <sky.french@diamond.ac.uk>
2019-06-13 14:22:38 +0100

    make available git description to odin build

--- .travis-install-odin.sh
+++ .travis-install-odin.sh
@@ -2,11 +2,11 @@
 VERSION="systemtest"
 ODIN="odin-data-${VERSION}"
 ODIN_PREFIX=$HOME/$ODIN
-wget https://github.com/dls-controls/odin-data/archive/${VERSION}.tar.gz
-tar xvfz ${VERSION}.tar.gz
-cd "$ODIN"
-mkdir build;
+git clone https://github.com/dls-controls/odin-data.git
+cd odin-data
+git checkout $VERSION
+mkdir -p build;
 cd build;
 cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -DCMAKE_INSTALL_PREFIX=$ODIN_PREFIX -DHDF5_ROOT=$HDF5_ROOT ..
-make -j 4 VERBOSE=1
+make -j 8 VERBOSE=1
 make install
