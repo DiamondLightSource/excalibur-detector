2ed953fabff0aadefbd0fc1fc1ff14692486cd36
Sky French <sky.french@diamond.ac.uk>
2019-06-13 12:09:46 +0100

    updating travis to run odinDataTest for excalibur.ini

---
+++ .travis-install-odin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+VERSION="systemtest"
+ODIN="odin-data-${VERSION}"
+ODIN_PREFIX=$HOME/$EXCALIBUR
+wget https://github.com/dls-controls/odin-data/archive/${VERSION}.tar.gz
+tar xvfz ${VERSION}.tar.gz
+cd "$ODIN"
+mkdir build;
+cd build;
+cmake -DCMAKE_INSTALL_PREFIX=$ODIN_PREFIX -DHDF5_ROOT=$HDF5_ROOT ..
+make -j 4 VERBOSE=1
+make install
