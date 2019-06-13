49cce726686a551a37e211c0aacfc5881fb8560d
Sky French <sky.french@diamond.ac.uk>
2019-06-13 13:20:47 +0100

    further travis modifications

--- .travis-install-odin.sh
+++ .travis-install-odin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 VERSION="systemtest"
 ODIN="odin-data-${VERSION}"
-ODIN_PREFIX=$HOME/$EXCALIBUR
+ODIN_PREFIX=$HOME/$ODIN
 wget https://github.com/dls-controls/odin-data/archive/${VERSION}.tar.gz
 tar xvfz ${VERSION}.tar.gz
 cd "$ODIN"
 mkdir build;
 cd build;
-cmake -DCMAKE_INSTALL_PREFIX=$ODIN_PREFIX -DHDF5_ROOT=$HDF5_ROOT ..
+cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -DCMAKE_INSTALL_PREFIX=$ODIN_PREFIX -DHDF5_ROOT=$HDF5_ROOT ..
 make -j 4 VERBOSE=1
 make install
